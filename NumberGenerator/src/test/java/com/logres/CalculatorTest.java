dc9be229b32344cb68f655c6edc52c3b404a0b54
LOGRES <Logres.king@gmail.com>
2024-10-18 21:47:23 +0800

    DevOps

---
+++ NumberGenerator/src/test/java/com/logres/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 4);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(7, 4);
+        assertEquals(3, result);
+    }
+}
